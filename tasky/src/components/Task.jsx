1360cfeff4585d93e052adef19a8ecfc27e5fe62
warcaul <romzes200312@gmail.com>
2026-09-15 12:08:59 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,13 @@
+const Task = (props) => {
+
+    return (
+        <div  className="card">
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p>This is a task!</p>
+            <p>{props.children}</p>
+        </div>
+    )
+}
+
+export default Task;
