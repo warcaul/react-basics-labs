59c31abb2d5bec9b0cbdc684c6215f65d4896e69
warcaul <romzes200312@gmail.com>
2026-09-22 11:24:20 +0100

    Added task state

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -4,8 +4,8 @@ const Task = (props) => {
         <div  className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
-            <p>This is a task!</p>
             <p>{props.children}</p>
+            <p>{props.descroption}</p>
         </div>
     )
 }
