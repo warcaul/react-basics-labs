c48701ac59c18b07bd8f480a9a597f82c517b05b
warcaul <romzes200312@gmail.com>
2026-09-22 12:21:36 +0100

    Tasky Lab 2b - Exercises done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,12 +1,19 @@
 const Task = (props) => {
 
+    const priorityColor =
+        props.priorityLevel === 'High' ? 'red' :
+        props.priorityLevel === 'Medium' ? 'goldenrod' :
+        'green';
+
     return (
         <div className="card" style={{ backgroundColor: props.done ? 'lightgrey' : '#5bb4c4' }}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             <p>{props.description}</p>
-            <p>Priority level: <b>{props.priorityLevel}</b></p>
+            <p style={{ backgroundColor: priorityColor, color: 'white', borderRadius: '4px', padding: '6px' }}>
+                {props.priorityLevel}
+            </p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
