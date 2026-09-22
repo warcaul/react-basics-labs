7306c9d217cfba381a68fd88d64a87226a569350
warcaul <romzes200312@gmail.com>
2026-09-22 11:54:36 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -20,6 +20,13 @@ function App() {
     setTaskState({ tasks });
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({ tasks });
+  }
+
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -34,6 +41,8 @@ function App() {
           priorityLevel={task.priorityLevel}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask={() => deleteHandler(index)}
+
         />
       ))}
 
